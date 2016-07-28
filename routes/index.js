547e33f4d19d87ecb10c33e9c739f67cab1f8132
Nabil Muthanna <nabilmuthanna@Nabils-Mac-mini.local>
2016-07-28 13:21:32 -0600

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+//GET /
+router.get('/register', function(req, res, next) {
+  res.send('register');
+});
+
+// GET /
+router.get('/', function(req, res, next) {
+  return res.render('index', { title: 'Home' });
+});
+
+// GET /about
+router.get('/about', function(req, res, next) {
+  return res.render('about', { title: 'About' });
+});
+
+// GET /contact
+router.get('/contact', function(req, res, next) {
+  return res.render('contact', { title: 'Contact' });
+});
+
+module.exports = router;
